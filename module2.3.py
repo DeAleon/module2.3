891f5fe4fea56b22a5be70bf6e42488e1238623e
DeAleon <donnotel@mail.ru>
2024-07-05 14:05:26 +0300

    Add files via upload

---
+++ module2.3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+nember = 0
+while nember < len(my_list):
+    if my_list[nember] >= 0:
+        print(my_list[nember])
+        nember += 1
+    else:
+        break
+
+
+
+
